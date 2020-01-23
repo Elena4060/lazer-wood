43ab191926492a43fa5566b0b6e3fd25eebdbaff
Elena4060 <itelena95@gmail.com>
2020-01-23 21:38:40 +0300

    sdfg

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+$('.menu-button').on('click', function () {
+	$('.menu').toggleClass('menu_active');
+});
+
+$('.menu__link').on('click', function () {
+	$('.menu').toggleClass('menu_active');
+});
+
