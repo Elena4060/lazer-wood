5331e33e7bd2a47f2519f96fae57240e2898b45b
Elena4060 <itelena95@gmail.com>
2020-01-28 22:36:29 +0300

    dfghjkl

--- js/jquery.js
+++ js/jquery.js
@@ -6,4 +6,21 @@ $(document).ready(function(){
 	$('.menu__link').on('click', function () {
 		$('.menu').toggleClass('menu_active');
 	})
+
+	$('#submit').click(function() {
+		var name = $('#name').val();
+		var phone = $('#phone').val();
+
+		var varData = 'Имя' + name + ' Телефон:' + phone;
+		console.log(varData);
+		$.ajax({
+			type:'POST',
+			url:'mail.php',
+			data: varData,
+			success: function(){
+				alert ("Спасибо!");
+			}
+		});
+
+	});
 });	
--- mail.php
+++ mail.php
@@ -17,35 +17,36 @@ $price = $_POST['price_user'];
 // };
 
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+//$mail->SMTPDebug = 3;                               
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'alexalexalexpol@mail.ru'; // Р’Р°С€ Р»РѕРіРёРЅ РѕС‚ РїРѕС‡С‚С‹ СЃ РєРѕС‚РѕСЂРѕР№ Р±СѓРґСѓС‚ РѕС‚РїСЂР°РІР»СЏС‚СЊСЃСЏ РїРёСЃСЊРјР°
-$mail->Password = 'gionr7wefklknjn'; // Р’Р°С€ РїР°СЂРѕР»СЊ РѕС‚ РїРѕС‡С‚С‹ СЃ РєРѕС‚РѕСЂРѕР№ Р±СѓРґСѓС‚ РѕС‚РїСЂР°РІР»СЏС‚СЊСЃСЏ РїРёСЃСЊРјР°
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / СЌС‚РѕС‚ РїРѕСЂС‚ РјРѕР¶РµС‚ РѕС‚Р»РёС‡Р°С‚СЊСЃСЏ Сѓ РґСЂСѓРіРёС… РїСЂРѕРІР°Р№РґРµСЂРѕРІ
+// $mail->isSMTP();                                      
+// $mail->Host = 'smtp.mail.ru';  																							
+// $mail->SMTPAuth = true;                               
+// $mail->Username = 'alexalexalexpol@mail.ru'; 
+// $mail->Password = 'gionr7wefklknjn'; 
+// $mail->SMTPSecure = 'ssl';                            
+// $mail->Port = 465; 
 
-$mail->setFrom('alexalexalexpol@mail.ru'); // РѕС‚ РєРѕРіРѕ Р±СѓРґРµС‚ СѓС…РѕРґРёС‚СЊ РїРёСЃСЊРјРѕ?
-$mail->addAddress('lazer.wooden@gmail.com');     // РљРѕРјСѓ Р±СѓРґРµС‚ СѓС…РѕРґРёС‚СЊ РїРёСЃСЊРјРѕ 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+// $mail->setFrom('alexalexalexpol@mail.ru');
+$mail->addAddress('lazer.wooden@gmail.com');       
+// //$mail->addAddress('ellen@example.com');               // Name is optional
+// //$mail->addReplyTo('info@example.com', 'Information');
+// //$mail->addCC('cc@example.com');
+// //$mail->addBCC('bcc@example.com');
+// //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+// //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+// $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Заявка с сайта Laser&Wooden';
-$mail->Body    = '' .$name . ' оставил заявку.<br> Его номер телефона: ' .$phone. 
-'Материал: ' .$material. '.<br> Услуга: ' .$typeorder. '.<br> Описани: '.$desctiprtion.
-'.<br> Предлагает цену: '.$price. 'Дополнительно хочет:' ;
-$mail->AltBody = '';
+// $mail->Subject = 'Заявка с сайта Laser&Wooden';
+// $mail->Body    = '' .$name . ' оставил заявку.<br> Его номер телефона: ' .$phone. 
+// 'Материал: ' .$material. '.<br> Услуга: ' .$typeorder. '.<br> Описани: '.$desctiprtion.
+// '.<br> Предлагает цену: '.$price. 'Дополнительно хочет:' ;
+// $mail->AltBody = '';
 
-if(!$mail->send()) {
-    echo 'Error';
-} else {
-    header('location: thank-you.html');
-}
-?>
+// if(!$mail->send()) {
+//     echo 'Error';
+// } else {
+//     header('location: thank-you.html');
+// }
+mail('lazer.wooden@gmail.com', $name, $phone,"Спасибо" );
+ ?>
